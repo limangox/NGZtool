bf1105e5f912ef993e3a3944b33a76d587d1e00a
melomen <30689352+limangox@users.noreply.github.com>
2024-05-13 15:50:52 +0900

    Update N46toolbox.py

--- N46toolbox.py
+++ N46toolbox.py
@@ -731,25 +731,6 @@ def news_catch():
             i += 1
         st.markdown(img_contnt, unsafe_allow_html=True)
 
-    def get_rajira_blog(url):
-        title, image_urls = rajira(url)
-        # 创建压缩文件并下载
-        if st.button("下载图片"):
-            st.info('请稍等,正在将图片处理至压缩包')
-            zip_filename = create_zip(title, image_urls)
-            with open(zip_filename, "rb") as f:
-                bytes_data = f.read()
-            st.success('压缩完整,请点击下载')
-            st.download_button(label="点击下载", data=bytes_data, file_name=zip_filename)
-        st.title(title)
-        i = 0
-        img_contnt = '<div style="display:inline">'
-        for img in range(len(image_urls)):
-            pic = image_urls[i]
-            img_contnt += f'''<img src='{pic}' width="30%">'''
-            i += 1
-        st.markdown(img_contnt, unsafe_allow_html=True)
-
     if 'nikkansports' in news_url:
         nikkansports(news_url)
     if 'oricon' in news_url:
