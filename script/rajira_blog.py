04d8d69252198dfecd95f4f80abc09f8fca82e42
melomen <30689352+limangox@users.noreply.github.com>
2024-05-13 15:20:18 +0900

    Update rajira_blog.py

--- script/rajira_blog.py
+++ script/rajira_blog.py
@@ -8,7 +8,6 @@ def rajira(url):
     headers = {
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
         'accept-language': 'zh-CN,zh;q=0.9,ja;q=0.8,ko;q=0.7,en;q=0.6,tr;q=0.5,ru;q=0.4',
-        'cookie': 'AMCV_F2EE53755E31533C0A495F9D%40AdobeOrg=179643557%7CMCMID%7C18818877612564026475558796745638989303%7CMCAID%7CNONE%7CMCOPTOUT-1712496083s%7CNONE%7CvVersion%7C5.5.0; s_cc=true; s_ips_orgc=2510; s_tp_orgc=6120; s_ppv_orgc=p%253Aradirer%253Ars%253A88W859PYQ9%253Ablog%253Abl%253Ap30Z7R1ZY2%253Aindex%2C100%2C41%2C6120%2C7%2C7; s_sq=%5B%5BB%5D%5D',
         'referer': f'{url}',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
     }
