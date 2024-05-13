2225f27f2a0809b907514d881ff6b45127454e93
melomen <30689352+limangox@users.noreply.github.com>
2024-05-13 19:18:31 +0900

    Update rajira_blog.py

--- script/rajira_blog.py
+++ script/rajira_blog.py
@@ -23,6 +23,8 @@ def rajira(url):
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
     }
 
+    st.write(headers)
+
     resp = requests.get(
         url,
         headers=headers,
